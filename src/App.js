3fd585ad11fd28c1f53a36681b0e54bbea5b0745
Lilia Mabel Borrazás <lilia.borrazas@ultimasoftware.com>
2019-09-27 15:12:29 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+//import { Grid } from 'office-ui-fabric-react';
+
 
 function App() {
+  const [data, setData] = useState({});
+  const { title, explanation, url } = data;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios (
+        'https://api.nasa.gov/planetary/apod?api_key=MxUtdcM3GzaXzE5K3obxNxC2hEol2r6TQnH8Oet8',
+        );
+        setData(result.data);
+      };
+      fetchData();
+    }, []);
+    
+  
+    console.log({data});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App ms-Grid">
+      <header className="header ms-Grid-row" >
+        <h1>Las super nenas galácticas</h1>
+        <img className="ms-Grid-col ms-sm12 image_header" src={url} alt="Sam"/>
       </header>
+      <main>
+        <h2>{title}</h2>
+        <p>{explanation}</p>
+        </main>    
     </div>
   );
 }
